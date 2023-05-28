f7bb4bbd81f0c2d93519b6db685c3ed500ba4a65
Wandji69 <collinschuwa@gmail.com>
2023-05-28 11:16:58 +0100

    Added bot file with basic structure

---
+++ bot.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+
+
+chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+trainer.train()
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🪴 {chatbot.get_response(query)}")
