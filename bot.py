5db1be888ebad87db5db303db2ad3340629dbe6e
Wandji69 <collinschuwa@gmail.com>
2023-05-30 02:21:23 +0100

    using cleaner file to clean whatsApp chat and use it as a corpus

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from cleaner import clean_corpus
+"""
+# import spacy
+# from spacy.lang.en.examples import sentences 
 
+# nlp = spacy.load("en_core_web_sm")
+# doc = nlp(sentences[0])
+"""
 
+CORPUS_FILE = "chat.txt"
 
 chatbot = ChatBot("Chatpot")
 
 trainer = ListTrainer(chatbot)
-trainer.train()
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
+# trainer.train(doc.text)
 
 exit_conditions = (":q", "quit", "exit")
 while True:
